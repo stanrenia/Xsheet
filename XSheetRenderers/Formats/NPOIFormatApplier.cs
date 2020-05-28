5e729e60fb1d081341f1934552151614c5eb5638
Stan Renia <mrkepers@gmail.com>
2020-05-27 23:13:47 -1000

    rm automapper

--- XSheetRenderers/Formats/NPOIFormatApplier.cs
+++ XSheetRenderers/Formats/NPOIFormatApplier.cs
@@ -1,44 +1,12 @@
-﻿using AutoMapper;
-using NPOI.SS.UserModel;
-using System.Collections.Generic;
-using System.Linq;
+﻿using NPOI.SS.UserModel;
 
 namespace XSheet.Renderers.Formats
 {
     public class NPOIFormatApplier : BaseFormatApplier<NPOIFormat>
     {
-        private readonly Mapper _mapper = new Mapper(new MapperConfiguration(conf =>
-        {
-            conf.CreateMap<NPOIFormat, NPOIFormat>()
-            .ForAllMembers(source =>
-            {
-                source.Condition((sourceObject, destObject, sourceProperty, destProperty) =>
-                {
-                    var initial = default(NPOIFormat);
-                    if (sourceProperty == null)
-                        return !(destProperty == null);
-
-                    return !sourceProperty.Equals(destProperty);
-                });
-            });
-        }));
-        
         public override void ApplyFormatToCell(IWorkbook wb, ICell cell, NPOIFormat format)
         {
             cell.CellStyle = format.CellStyle;
         }
-
-        //protected override NPOIFormat MergeFormats(List<NPOIFormat> formats)
-        //{
-        //    var firstFormat = formats?.FirstOrDefault() ?? new NPOIFormat { CellStyle = null };
-        //    return formats.Aggregate(firstFormat, (mergedFormat, nextFormat) =>
-        //    {
-        //        if (nextFormat.CellStyle != null)
-        //        {
-        //            _mapper.Map(nextFormat, mergedFormat);
-        //        }
-        //        return mergedFormat;
-        //    });
-        //}
     }
 }
