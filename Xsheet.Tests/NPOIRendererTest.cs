c149ccccdb1372a92ac4dab8ff9f874e1a923578
Stan Renia <mrkepers@gmail.com>
2020-05-27 23:04:46 -1000

    Repair test with NPOIFormat

--- Xsheet.Tests/NPOIRendererTest.cs
+++ Xsheet.Tests/NPOIRendererTest.cs
@@ -18,11 +18,11 @@ namespace Xsheet.Tests
         private readonly FormatApplier _defaultFormatApplier;
         private readonly IMatrixRenderer _renderer;
         private readonly List<Stream> _fileStreamToClose;
-        private const string FILE_DEBUG = "debug.xlsx";
-        private const string FILE_DEBUG2 = "debug2.xlsx";
-        private const string FILE_TEST_1 = "test1.xlsx";
-        private const string FILE_TEST_FORMAT_BASIC = "test_format_basic.xlsx";
-        private const string FILE_TEST_FORMAT_NPOI = "test_format_npoi.xlsx";
+        private const string FILE_DEBUG = "debug";
+        private const string FILE_DEBUG2 = "debug2";
+        private const string FILE_TEST_1 = "test1";
+        private const string FILE_TEST_FORMAT_BASIC = "test_format_basic";
+        private const string FILE_TEST_FORMAT_NPOI = "test_format_npoi";
 
         public NPOIRendererTest()
         {
@@ -237,32 +237,47 @@ namespace Xsheet.Tests
             const string Firstname = "Firstname";
             const string Age = "Age";
 
+            IFont Font1()
+            {
+                var f = _workbook.CreateFont();
+                f.IsItalic = true;
+                return f;
+            }
+
+            IFont Font2()
+            {
+                var f = _workbook.CreateFont();
+                f.FontHeightInPoints = 12;
+                return f;
+            }
+
+            IFont Font3()
+            {
+                var f = Font2();
+                f.IsBold = true;
+                return f;
+            }
+
             var style1 = _workbook.CreateCellStyle();
-            var font1 = _workbook.CreateFont();
-            font1.IsItalic = true;
-            style1.SetFont(font1);
+            style1.SetFont(Font1());
 
             var style2 = _workbook.CreateCellStyle();
-            var font2 = _workbook.CreateFont();
-            font2.FontHeightInPoints = 12;
-            style2.SetFont(font2);
+            style2.SetFont(Font2());
 
             var style3 = _workbook.CreateCellStyle();
-            var font3 = _workbook.CreateFont();
             style3.CloneStyleFrom(style2);
-            font3.CloneStyleFrom(font2);
-            font3.IsBold = true;
-            style3.SetFont(font3);
+            style3.SetFont(Font3());
 
             var ColorBlueIndex = IndexedColors.LightBlue.Index;
             var ColorLightGreyIndex = IndexedColors.Grey25Percent.Index;
             
             var style4 = _workbook.CreateCellStyle();
             style4.CloneStyleFrom(style2);
+            style4.FillPattern = FillPattern.SolidForeground;
             style4.FillForegroundColor = ColorLightGreyIndex;
 
             var style5 = _workbook.CreateCellStyle();
-            style5.CloneStyleFrom(style2);
+            style5.CloneStyleFrom(style4);
             style5.FillForegroundColor = ColorBlueIndex;
 
             var cols = new List<ColumnDefinition>
@@ -378,37 +393,69 @@ namespace Xsheet.Tests
         public void Should_Help_To_Debug_NPOIFormat()
         {
             var wb = new XSSFWorkbook();
-
-            var s1 = wb.CreateCellStyle();
-            s1.FillForegroundColor = IndexedColors.LightBlue.Index;
-            var f1 = wb.CreateFont();
-            f1.Color = IndexedColors.Black.Index;
-            f1.FontHeightInPoints = 14;
-            f1.IsBold = true;
-            s1.SetFont(f1);
-
-            var s2 = wb.CreateCellStyle();
-            var f2 = wb.CreateFont();
-            s2.CloneStyleFrom(s1);
-            s2.FillForegroundColor = IndexedColors.LightOrange.Index;
-            f2.CloneStyleFrom(f1);
-            f2.IsItalic = true;
-            f2.Color = IndexedColors.Green.Index;
-            s2.SetFont(f2);
-
-            // FIXME f1.Color = f2.Color!!
-            Check.That(f1.Color).IsNotEqualTo(f2.Color);
             
+            short colorBlack = IndexedColors.Black.Index;
+            short colorLightBlue = IndexedColors.LightBlue.Index;
+            short colorLightOrange = IndexedColors.LightOrange.Index;
+            short colorGreen = IndexedColors.Green.Index;
+
             var sheet = wb.CreateSheet();
             var row0 = sheet.CreateRow(0);
             var cell0 = row0.CreateCell(0);
-            cell0.CellStyle = s1;
             cell0.SetCellValue("FIRST");
             var cell1 = row0.CreateCell(1);
-            cell1.CellStyle = s2;
             cell1.SetCellValue("SECOND");
 
-            WriteDebugFileAndClose(wb, FILE_DEBUG2);
+            // Best way to clone Font: Wrap its creation in a method
+            // Because the existing method IFont.CloneStyleFrom(IFont) is buggy
+            IFont GetFont1()
+            {
+                var f = wb.CreateFont();
+                f.Color = colorBlack;
+                f.FontHeightInPoints = 14;
+                f.IsBold = true;
+                return f;
+            }
+
+            var s1 = wb.CreateCellStyle();
+            s1.FillPattern = FillPattern.SolidForeground;
+            s1.FillForegroundColor = colorLightBlue;
+            s1.VerticalAlignment = VerticalAlignment.Bottom;
+            var f1 = GetFont1();
+            s1.SetFont(f1);
+            cell0.CellStyle = s1;
+
+            var s2 = wb.CreateCellStyle();
+            s2.CloneStyleFrom(s1);
+            s2.FillForegroundColor = colorLightOrange;
+            s2.Rotation = 49;
+            s1.VerticalAlignment = VerticalAlignment.Top;
+            var f2 = GetFont1();
+            f2.IsBold = false;
+            f2.IsItalic = true;
+            f2.Color = colorGreen;
+            s2.SetFont(f2);
+            cell1.CellStyle = s2;
+
+            WriteDebugFile(wb, FILE_DEBUG2);
+
+            using (var ms = new MemoryStream())
+            {
+                wb.Write(ms);
+
+                var readWb = new XSSFWorkbook(new MemoryStream(ms.ToArray()));
+                var readSheet = readWb.GetSheetAt(0);
+                var rs1 = readSheet.GetRow(0).GetCell(0).CellStyle;
+                Check.That(rs1).IsNotNull();
+                Check.That(rs1.FillForegroundColor).IsEqualTo(colorLightBlue);
+                Check.That(rs1.GetFont(readWb).IsBold).IsTrue();
+                Check.That(rs1.GetFont(readWb).IsItalic).IsFalse();
+                Check.That(rs1.GetFont(readWb).Color).IsEqualTo(colorBlack);
+                var rs2 = readSheet.GetRow(0).GetCell(1).CellStyle;
+                Check.That(rs2.GetFont(readWb).IsBold).IsFalse();
+                Check.That(rs2.GetFont(readWb).IsItalic).IsTrue();
+                Check.That(rs2.GetFont(readWb).Color).IsEqualTo(colorGreen);
+            }
         }
 
         private List<ICell> ReadAllCells(IWorkbook readWb, int sheetIndex)
@@ -429,21 +476,27 @@ namespace Xsheet.Tests
             WriteDebugFile(_defaultFormatApplier, mat, fileName);
         }
 
-        private void WriteDebugFile(FormatApplier formatApplier, Matrix mat, string fileName, IWorkbook wb = null)
+        private void WriteDebugFile(FormatApplier formatApplier, Matrix mat, string filename, IWorkbook wb = null)
         {
             if (wb is null)
             {
                 wb = new XSSFWorkbook();
             }
             var rd = new NPOIRenderer(wb, formatApplier);
-            var fs = File.Create(fileName);
+            var fs = File.Create($"{filename}_{DateTime.Now.ToString("HHmmss")}.xlsx");
             _fileStreamToClose.Add(fs);
             rd.GenerateExcelFile(mat, fs);
         }
 
+        private void WriteDebugFile(IWorkbook wb, string filename)
+        {
+            var fs = File.Create($"{filename}_{DateTime.Now.ToString("HHmmss")}.xlsx");
+            wb.Write(fs);
+        }
+
         private void WriteDebugFileAndClose(IWorkbook wb, string filename)
         {
-            var fs = File.Create(filename);
+            var fs = File.Create($"{filename}_{DateTime.Now.ToString("HHmmss")}.xlsx");
             wb.Write(fs);
             fs.Close();
             wb.Close();
