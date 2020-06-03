dacfac0b5241baf316a4a93bc9b13659f042ea34
Stan Renia <mrkepers@gmail.com>
2020-06-03 00:04:26 -1000

    WIP v1

---
+++ Xsheet.Tests/OpenXmlRendererTest.cs
@@ -0,0 +1,50 @@
+﻿using NFluent;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using XSheet.Renderers;
+using Xunit;
+
+namespace Xsheet.Tests
+{
+    public class OpenXmlRendererTest : IDisposable
+    {
+        private readonly OpenXmlRenderer _renderer;
+        private Stream _fileStream;
+
+        public OpenXmlRendererTest()
+        {
+            _renderer = new OpenXmlRenderer();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        [Fact]
+        public void Should_Render_Basic_Matrix()
+        {
+            // GIVEN
+            var mat = Matrix.With()
+                .RowValues(new List<RowValue>
+                {
+                    new RowValue { ValuesByColName = new Dictionary<string, object>
+                        {
+                            { "Lastname", "Bros" },
+                            { "Firstname", "Mario" },
+                        }
+                    }
+                })
+                .Build();
+
+            _fileStream = File.Create($"openxml_basic_{DateTime.Now:HHmmss}.xlsx");
+
+            // WHEN
+            _renderer.GenerateExcelFile(mat, _fileStream);
+
+            // THEN
+            Check.That(true).IsTrue();
+        }
+    }
+}
