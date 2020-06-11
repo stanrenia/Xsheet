051386c15393c83b11cbb1eae695adf25415da3b
Stan Renia <mrkepers@gmail.com>
2020-06-11 00:30:34 -1000

    use new Builder in DataSet

--- Xsheet.Tests/SharedDatasets/MatrixDatasets.cs
+++ Xsheet.Tests/SharedDatasets/MatrixDatasets.cs
@@ -47,48 +47,37 @@ namespace Xsheet.Tests.SharedDatasets
             const string Mean = "Mean";
             const string FinalTotal = "FinalTotal";
 
-            return Matrix.With().Key(index: index)
-                            .Cols(new List<ColumnDefinition> {
-                    new ColumnDefinition { Name = Playername, Label = "Player name" },
-                    new ColumnDefinition { Name = Score1, Label = "Score 1", DataType = DataTypes.Number },
-                    new ColumnDefinition { Name = Score2, Label = "Score 2", DataType = DataTypes.Number },
-                    new ColumnDefinition { Name = Score3, Label = "Score 3", DataType = DataTypes.Number },
-                    new ColumnDefinition { Name = Total, Label = "Total", DataType = DataTypes.Number },
-                    new ColumnDefinition { Name = Mean, Label = "Mean", DataType = DataTypes.Number },
-                            })
-                            .Rows(new List<RowDefinition> {
-                    new RowDefinition {
-                        ValuesMapping = new Dictionary<string, Func<Matrix, MatrixCellValue, object>> {
-                            { Total, (mat, cell) => {
-                                var row = mat.Row(cell);
-                                return Convert.ToDouble(row.Col(Score1).Value)
-                                + Convert.ToDouble(row.Col(Score2).Value)
-                                + Convert.ToDouble(row.Col(Score3).Value);
-                            }},
-                            { Mean, (mat, cell) => {
-                                var row = mat.Row(cell);
-                                return $"=AVERAGE({row.Col(Score1).Value},{row.Col(Score2).Value},{row.Col(Score3).Value})";
-                            } },
-                        }
-                    },
-                    new RowDefinition {
-                        Key = FinalTotal,
-                        DefaultCellFormat = format,
-                        ValuesMapping = new Dictionary<string, Func<Matrix, MatrixCellValue, object>>
+            return Matrix.With()
+                .Key(index: index)
+                .Cols()
+                    .Col(Playername, "Player name")
+                    .Col(Score1, "Score 1", DataTypes.Number)
+                    .Col(Score2, "Score 2", DataTypes.Number)
+                    .Col(Score3, "Score 3", DataTypes.Number)
+                    .Col(Total, "Total", DataTypes.Number)
+                    .Col(Mean, "Mean", DataTypes.Number)
+                .Rows()
+                    .Row()
+                        .ValueMap(Total, (mat, cell) =>
                         {
-                            { Playername, (mat, cell) => "TOTAL" },
-                            { Score1, (mat, cell) => mat.Col(cell).Values.Select(v => Convert.ToDouble(v)).Sum() },
-                            { Score2, (mat, cell) => $"=SUM({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Score2).Address})" },
-                            { Score3, (mat, cell) => {
-                                return $"={mat.Col(cell).Cells.SkipLast(1).Addresses("+")}";
-                            }},
-                            { Total, (mat, cell) => $"=SUM({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Total).Address})" },
-                            { Mean, (mat, cell) => $"=AVERAGE({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Mean).Address})" },
-                        }
-                    },
-                            })
-                            .RowValues(new List<RowValue>
-                            {
+                            var row = mat.Row(cell);
+                            return Convert.ToDouble(row.Col(Score1).Value)
+                            + Convert.ToDouble(row.Col(Score2).Value)
+                            + Convert.ToDouble(row.Col(Score3).Value);
+                        })
+                        .ValueMap(Mean, (mat, cell) =>
+                        {
+                            var row = mat.Row(cell);
+                            return $"=AVERAGE({row.Col(Score1).Value},{row.Col(Score2).Value},{row.Col(Score3).Value})";
+                        })
+                    .Row(FinalTotal, format)
+                        .ValueMap(Playername, (mat, cell) => "TOTAL")
+                        .ValueMap(Score1, (mat, cell) => mat.Col(cell).Values.Select(v => Convert.ToDouble(v)).Sum())
+                        .ValueMap(Score2, (mat, cell) => $"=SUM({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Score2).Address})")
+                        .ValueMap(Score3, (mat, cell) => $"={mat.Col(cell).Cells.SkipLast(1).Addresses("+")}")
+                        .ValueMap(Total, (mat, cell) => $"=SUM({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Total).Address})")
+                        .ValueMap(Mean, (mat, cell) => $"=AVERAGE({mat.Col(cell).Cells[0].Address}:{mat.RowAbove(cell).Col(Mean).Address})")
+                .RowValues(new List<RowValue> {
                     new RowValue { ValuesByColName = new Dictionary<string, object> {
                         { Playername, "Mario" }, { Score1, 10 }, { Score2, 20 }, { Score3, 30 }
                     } },
@@ -96,8 +85,8 @@ namespace Xsheet.Tests.SharedDatasets
                         { Playername, "Luigi" }, { Score1, 12 }, { Score2, 23 }, { Score3, 34 }
                     } },
                     new RowValue { Key = FinalTotal }
-                            })
-                            .Build();
+                })
+                .Build();
         }
     }
 }
